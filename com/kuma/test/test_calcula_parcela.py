9ac46fda80576984f186113b80bf5a03a4bc6c55
Izabella Queiroz Cardoso <1900693@SALAS.AULAS>
2019-09-20 22:20:54 -0300

    Testes e configuração travis

--- com/kuma/test/test_calcula_parcela.py
+++ com/kuma/test/test_calcula_parcela.py
@@ -2,7 +2,6 @@ import pytest
 from com.kuma.calcula_parcela import valorPagamento
 
 def test_parcela():
-    assert valorPagamento(-1,0) == None, "Should be None"
+	assert valorPagamento(-1,0) == None, "Should be None"
 	assert valorPagamento(500,5) == 540.00, "Should be 540.00"
-	assert valorPagamento(100,0) == 100.00 "Should be 100.00"
-    
+	assert valorPagamento(100,0) == 100.00, "Should be 100.00"
